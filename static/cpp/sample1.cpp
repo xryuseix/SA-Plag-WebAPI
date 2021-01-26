62b447c47c17bb23cb9fbc9ca7075bb8c7ad2395
xryuseix <ryusei143.shootingstar@gmail.com>
2021-01-26 12:43:54 +0900

    change sample code

--- static/cpp/sample1.cpp
+++ static/cpp/sample1.cpp
@@ -2,29 +2,31 @@
 #include <map>
 using namespace std;
 typedef long long int ll;
-#define irep(it, stl) for (auto it = stl.begin(); it != stl.end(); it++)
+#define irep(it, stl) for(auto it = stl.begin(); it != stl.end(); it++)
 
 map<ll, ll> prime;
 void factorize(int n) {
-    for (int i = 2; i * i <= n; i++) {
-        while (n % i == 0) {
+    for(int i = 2; i * i <= n; i++) {
+        while(n % i == 0) {
             prime[i]++;
             n /= i;
         }
     }
-    if (n != 1) {
+    if(n != 1) {
         prime[n] = 1;
     }
 }
 
+
 int main() {
+
     int n;
-    cin >> n;
-    int ans = n;
+    cin>>n;
+    int ans=n;
     factorize(n);
-    irep(i, prime) {
-        ans /= i->first;
-        ans *= i->first - 1;
+    irep(i,prime){
+        ans/=i->first;
+        ans*=i->first-1;
     }
     cout << (ans) << endl;
 }
--- static/cpp/sample2.cpp
+++ static/cpp/sample2.cpp
@@ -5,13 +5,13 @@ typedef long long int ll;
 
 map<ll, ll> prime;
 void factorize(int n) {
-    for (int i = 2; i * i <= n; i++) {
-        while (n % i == 0) {
+    for(int i = 2; i * i <= n; i++) {
+        while(n % i == 0) {
             prime[i]++;
             n /= i;
         }
     }
-    if (n != 1) {
+    if(n != 1) {
         prime[n] = 1;
     }
 }
@@ -19,9 +19,9 @@ void factorize(int n) {
 int main() {
     int n;
     cin >> n;
-    int ans = 0;
+    int ans  = 0;
     factorize(n);
-    for (auto it = prime.begin(); it != prime.end(); it++) {
+    for(auto it = prime.begin();it != prime.end(); it++) {
         ans = ans / it->first;
         ans = ans * (it->first - 1);
     }
