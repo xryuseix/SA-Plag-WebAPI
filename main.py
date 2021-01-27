0bfe3628c3534b9707f8281a93c8a4235d331bf5
xryuseix <ryusei143.shootingstar@gmail.com>
2021-01-27 15:55:51 +0900

    release pair api

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ app.mount(
 
 @app.post("/api")
 def infer(code_pair: List[str]):
+    # 最大30個まで受け付ける
     max_codes = 30
-    # 最大10個まで受け付ける
     if len(code_pair) > max_codes:
         code_pair = code_pair[:max_codes]
     # 最大10000文字で受け取る
@@ -43,6 +43,37 @@ def infer(code_pair: List[str]):
             res_json["result"].append(
                 {"pair": [i, j], "pred": int(pred), "prob": list(prob)}
             )
+    return res_json
+
+# [[a,b], [c,d], ...]の形で受け取る場合
+@app.post("/api/pair")
+def infer(code_pair: List[List[str]]):
+    # 最大100個まで受け付ける
+    max_codes = 100
+    if len(code_pair) > max_codes:
+        code_pair = code_pair[:max_codes]
+    
+    for pair in code_pair:
+        if len(pair) != 2:
+            return {"status" : "NG", "message" : "The size of the inner array must be 2."}
+    
+    # 最大10000文字で受け取る
+    code_pair = [[code[0][:10000], code[1][:10000]] for code in code_pair]
+
+    res_json = {"status": "OK", "result": []}
+
+    for i, (code1, code2) in enumerate(code_pair):
+        # 1文字の場合はトークン化がうまくできない
+        if len(code1) == 1 or len(code2) == 1:
+            res_json["result"].append(
+                {"pair": [i, i], "pred": int(1), "prob": [0, 1]}
+            )
+            continue
+        plag_class = plag.SA_Plag([code1, code2])
+        pred, prob = plag_class.get_predict_result()
+        res_json["result"].append(
+            {"pair": [i, i], "pred": int(pred), "prob": list(prob)}
+        )
     return res_json
 
 
